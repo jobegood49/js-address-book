6bf8b3e2b4e12aee3fbc766370286f411ae9384a
M Haidar Hanif <git@mhaidarhanif.com>
2019-02-01 12:04:49 +0700

    Improve code quality

--- index.js
+++ index.js
@@ -2,33 +2,33 @@ let contacts = [
   {
     id: 1,
     name: 'Mario Rizki',
-    phone: 08170209641,
+    phone: '08170209641'
   },
   {
     id: 2,
     name: 'Fadhila Fajrin Bramantio',
-    phone: 082112342326,
+    phone: '082112342326'
   },
   {
     id: 3,
     name: 'Bara Limbong',
-    phone: +17787231858,
+    phone: '+17787231858'
   },
   {
     id: 4,
     name: 'Jonathan Nicolas ',
-    phone: 085770346560,
+    phone: '085770346560'
   },
   {
     id: 5,
     name: 'Muhammad Fahrianda',
-    phone: +6285297161214,
+    phone: '+6285297161214'
   },
   {
     id: 6,
     name: 'Sakti Dewantoro',
-    phone: +6285743820777,
-  },
+    phone: '+6285743820777'
+  }
 ]
 
 let currentId = 6
@@ -37,33 +37,36 @@ let addressBook = {
   displayContacts: contacts => {
     contacts.forEach(contact => console.log(contact))
   },
+
   addContact: contact => {
-    if (!contact.name || !contact.phone)
-      return console.log('name or phone is missing')
-    currentId += 1
-    contact.id = currentId
-    contacts.push(contact)
+    if (!contact.name || !contact.phone) {
+      console.log('either name and phone is missing')
+    } else {
+      currentId += 1
+      contact.id = currentId
+      contacts.push(contact)
+    }
   },
-  searchContact: name => {
-    let foundContact = contacts.filter(contact => {
-      return contact[name] === name
+
+  searchContactByName: name => {
+    const foundContact = contacts.filter(contact => {
+      return contact.name.toLowerCase().includes(name.toLowerCase())
     })
-    console.log(foundContact)
     console.log('Details of the person are :', foundContact)
-  },
+  }
 }
 
-addressBook.displayContacts(contacts)
+// addressBook.displayContacts(contacts)
 
 addressBook.addContact({
-  number: 'toto-number',
+  number: 'toto-number'
 })
 
 addressBook.addContact({
   name: 'toto',
-  phone: 'toto-number',
+  phone: 'toto-number'
 })
 
-addressBook.displayContacts(contacts)
+// addressBook.displayContacts(contacts)
 
-addressBook.searchContact('Jonathan Nicolas')
+addressBook.searchContactByName('Jonathan Nicolas')
