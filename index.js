956dbf42e1ddb358bfc0c372c848c0dfb9836281
M Haidar Hanif <git@mhaidarhanif.com>
2019-02-04 11:32:39 +0700

    Improve code quality

--- index.js
+++ index.js
@@ -2,33 +2,33 @@ let contacts = [
   {
     id: 1,
     name: 'Mario Rizki',
-    phone: '08170209641',
+    phone: '08170209641'
   },
   {
     id: 2,
     name: 'Fadhila Fajrin Bramantio',
-    phone: '082112342326',
+    phone: '082112342326'
   },
   {
     id: 3,
     name: 'Bara Limbong',
-    phone: '+17787231858',
+    phone: '+17787231858'
   },
   {
     id: 4,
     name: 'Jonathan Nicolas ',
-    phone: '085770346560',
+    phone: '085770346560'
   },
   {
     id: 5,
     name: 'Muhammad Fahrianda',
-    phone: '+6285297161214',
+    phone: '+6285297161214'
   },
   {
     id: 6,
     name: 'Sakti Dewantoro',
-    phone: '+6285743820777',
-  },
+    phone: '+6285743820777'
+  }
 ]
 
 let currentId = 6
@@ -37,12 +37,12 @@ const contactList = document.getElementById('contact-list')
 let addressBook = {
   displayContacts: contacts => {
     contacts.forEach(contact => {
-      const liAddressBook = document.createElement('li')
+      const li = document.createElement('li')
       const text = document.createTextNode(
-        'name: ' + contact.name + '/' + 'phone: ' + contact.phone
+        `Name: ${contact.name} / Phone: ${contact.phone}`
       )
-      liAddressBook.appendChild(text)
-      contactList.appendChild(liAddressBook)
+      li.appendChild(text)
+      contactList.appendChild(li)
     })
   },
 
@@ -52,13 +52,13 @@ let addressBook = {
     const newContactPhone = document.getElementById('phone-number').value
 
     if (!newContactName || !newContactPhone) {
-      alert('either name and phone is missing')
+      alert('Either name and phone is missing')
     } else {
       currentId += 1
       contacts.push({
         id: currentId,
         name: newContactName,
-        phone: newContactPhone,
+        phone: newContactPhone
       })
       contactList.innerHTML = ''
 
@@ -71,7 +71,7 @@ let addressBook = {
       return contact.name.toLowerCase().includes(name.toLowerCase())
     })
     console.log('Details of the person are :', foundContact)
-  },
+  }
 }
 
 addressBook.displayContacts(contacts)
